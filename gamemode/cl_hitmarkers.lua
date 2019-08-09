9be0c790510ef1e34d6954236c4bc38fe663dc68
Meepen <meepdarknessmeep@gmail.com>
2019-08-09 12:24:39 -0400

    add actual hitmarkers lol

--- gamemode/cl_hitmarkers.lua
+++ gamemode/cl_hitmarkers.lua
@@ -1,15 +1,64 @@
+local last_hit = -math.huge
+
 function GM:PlayerHit(atk, dmg, dmgtype, hitgroup)
     if (atk ~= LocalPlayer()) then
         return
     end
 
+    last_hit = CurTime()
+
     sound.PlayFile(hitgroup == HITGROUP_HEAD and "sound/tttrw/hitmarker_hs_.mp3" or "sound/tttrw/hitmarker_.mp3", "mono", function(station, eid, err)
         if (IsValid(station)) then
-            print(hitgroup == HITGROUP_HEAD)
-            station:SetVolume(5)
+            station:SetVolume(2)
             station:Play()
         else
             MsgC(Color(255,0,0,255,err))
         end
     end)
+end
+
+local color = Color(220, 220, 220, 255)
+local black = Color(0,0,0,255)
+
+function GM:HUDPaint()
+
+    local alpha = 1 - (CurTime() - last_hit) / 0.5
+
+    if (alpha <= 0) then
+        return
+    end
+
+    alpha = alpha * 255
+    color.a = alpha
+    black.a = alpha
+
+    local w, h = ScrW(), ScrH()
+    local x, y = math.floor(w / 2), math.floor(h / 2)
+    surface.SetDrawColor(color)
+
+    for i = -1, 1 do
+        surface.DrawLine(x - 15 + i, y - 15, x - 5 + i, y - 5)
+        surface.DrawLine(x - 15 + i, y + 15, x - 5 + i, y + 5)
+        surface.DrawLine(x + 15 + i, y - 15, x + 5 + i, y - 5)
+        surface.DrawLine(x + 15 + i, y + 15, x + 5 + i, y + 5)
+    end
+
+    surface.SetDrawColor(color_black)
+
+    for i = -2, 2, 4 do
+        surface.DrawLine(x - 15 + i, y - 15, x - 5 + i, y - 5)
+        surface.DrawLine(x - 15 + i, y + 15, x - 5 + i, y + 5)
+        surface.DrawLine(x + 15 + i, y - 15, x + 5 + i, y - 5)
+        surface.DrawLine(x + 15 + i, y + 15, x + 5 + i, y + 5)
+    end
+
+    surface.DrawLine(x - 18, y - 16, x - 13, y - 16)
+    surface.DrawLine(x + 18, y - 16, x + 13, y - 16)
+    surface.DrawLine(x - 18, y + 16, x - 13, y + 16)
+    surface.DrawLine(x + 18, y + 16, x + 13, y + 16)
+
+    surface.DrawLine(x - 8, y - 6, x - 3, y - 6)
+    surface.DrawLine(x + 8, y - 6, x + 3, y - 6)
+    surface.DrawLine(x - 8, y + 6, x - 3, y + 6)
+    surface.DrawLine(x + 8, y + 6, x + 3, y + 6)
 end
