f743319f73d3bfda8e6c20cd0ef8982e366d2284
Meepen <meepdarknessmeep@gmail.com>
2019-08-09 14:50:11 -0400

    change this to tabs

--- gamemode/sh_files.lua
+++ gamemode/sh_files.lua
@@ -1,52 +1,52 @@
 AddCSLuaFile();
 
 ({
-    Server = {
-        "sv_player_management.lua",
-        "sv_round_system.lua",
-        "sv_spawning.lua",
-        "sv_spectator.lua",
-        "sv_ragdoll.lua",
-        "sv_chat.lua",
-        "sv_hitmarkers.lua",
-    },
-    Client = {
-        "cl_hud_helpers.lua",
-        "cl_hud.lua",
-        "cl_scoreboard.lua",
-        "cl_hitmarkers.lua",
-    },
-    Shared = {
-        "sh_init.lua",
-        "libraries/quaternion.lua",
-        "player_terror.lua",
-        "sh_roles.lua",
-        "sh_proper_networking.lua",
-        "sh_round_system.lua",
-        "sh_spectator.lua",
-        "player_terror.lua",
-        "sh_movement.lua",
+	Server = {
+		"sv_player_management.lua",
+		"sv_round_system.lua",
+		"sv_spawning.lua",
+		"sv_spectator.lua",
+		"sv_ragdoll.lua",
+		"sv_chat.lua",
+		"sv_hitmarkers.lua",
+	},
+	Client = {
+		"cl_hud_helpers.lua",
+		"cl_hud.lua",
+		"cl_scoreboard.lua",
+		"cl_hitmarkers.lua",
+	},
+	Shared = {
+		"sh_init.lua",
+		"libraries/quaternion.lua",
+		"player_terror.lua",
+		"sh_roles.lua",
+		"sh_proper_networking.lua",
+		"sh_round_system.lua",
+		"sh_spectator.lua",
+		"player_terror.lua",
+		"sh_movement.lua",
 		"sh_equipment.lua",
 		"sh_util.lua"
-    },
-    Load = function(self)
-        for _, file in ipairs(self.Shared) do
-            if (SERVER) then
-                AddCSLuaFile(file)
-            end
-            include(file)
-        end
+	},
+	Load = function(self)
+		for _, file in ipairs(self.Shared) do
+			if (SERVER) then
+				AddCSLuaFile(file)
+			end
+			include(file)
+		end
 
-        for _, file in ipairs(SERVER and self.Server or self.Client) do
-            include (file)
-        end
+		for _, file in ipairs(SERVER and self.Server or self.Client) do
+			include (file)
+		end
 
-        if (not SERVER) then
-            return
-        end
+		if (not SERVER) then
+			return
+		end
 
-        for _, file in ipairs(self.Client) do
-            AddCSLuaFile(file)
-        end
-    end
+		for _, file in ipairs(self.Client) do
+			AddCSLuaFile(file)
+		end
+	end
 }):Load()
