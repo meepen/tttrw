81a78d480463d1fe5fdc8098da97363633f1050d
Meepen <meepdarknessmeep@gmail.com>
2019-08-11 17:52:59 -0400

    Fix crosshair menu

--- entities/weapons/weapon_tttbase/cl_init.lua
+++ entities/weapons/weapon_tttbase/cl_init.lua
@@ -35,16 +35,6 @@ function SWEP:DrawHUD()
 		surface.DrawRect(math.floor(x + ScrH() / 2), 0, math.ceil(x - ScrH() / 2), ScrH())
 	end
 end
-local tttrw_crosshair_color_r = CreateConVar("tttrw_crosshair_color_r", 232, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
-local tttrw_crosshair_color_g = CreateConVar("tttrw_crosshair_color_g", 80, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
-local tttrw_crosshair_color_b = CreateConVar("tttrw_crosshair_color_b", 94, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
-local tttrw_crosshair_thickness = CreateConVar("tttrw_crosshair_thickness", 2, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
-local tttrw_crosshair_length = CreateConVar("tttrw_crosshair_length", 7, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
-local tttrw_crosshair_gap = CreateConVar("tttrw_crosshair_gap", 3, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
-local tttrw_crosshair_opacity = CreateConVar("tttrw_crosshair_opacity", 255, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
-local tttrw_crosshair_outline_opacity = CreateConVar("tttrw_crosshair_outline_opacity", 255, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
-local tttrw_crosshair_dot_size = CreateConVar("tttrw_crosshair_dot_size", 0, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
-local tttrw_crosshair_dot_opacity = CreateConVar("tttrw_crosshair_dot_opacity", 255, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
 
 local function drawCircle(x, y, radius, seg)
 	local cir = {}
@@ -75,41 +65,8 @@ function SWEP:DoDrawCrosshair(x, y)
 
 		return true
 	end
-
-	local r = tttrw_crosshair_color_r:GetInt()
-	local g = tttrw_crosshair_color_g:GetInt()
-	local b = tttrw_crosshair_color_b:GetInt()
-	local t = tttrw_crosshair_thickness:GetInt()
-	local len = tttrw_crosshair_length:GetInt()
-	local gap = tttrw_crosshair_gap:GetInt()*2
-	local opacity = tttrw_crosshair_opacity:GetInt()
-	local oopacity = tttrw_crosshair_outline_opacity:GetInt()
-	local dot = tttrw_crosshair_dot_size:GetInt()
-	local dopacity = tttrw_crosshair_dot_opacity:GetInt()
-
-	local s = len * 2 + gap
-	local startw = x - s / 2
-	local starth = y - s / 2
-	if (len > 0 and t > 0) then
-		surface.SetDrawColor(0, 0, 0, oopacity * 255) -- outlines, counterclockwise
-		surface.DrawRect(x - t / 2 - 1, starth - 1, t + 2, len + 2)
-		surface.DrawRect(startw - 1 , y- t / 2 - 1, len + 2, t + 2)
-		surface.DrawRect(x - t / 2 - 1, starth + s - len - 1, t + 2, len + 2)
-		surface.DrawRect(startw + s - len - 1, y - t / 2 - 1, len + 2, t + 2)
-		surface.SetDrawColor(r, g, b, opacity) -- crosshairs, counterclockwise
-		surface.DrawRect(x - t / 2, starth, t, len)
-		surface.DrawRect(startw, y - t / 2, len, t)
-		surface.DrawRect(x - t / 2, starth + s - len, t, len)
-		surface.DrawRect(startw + s - len, y - t / 2, len, t)
-	end
-
-	if (dot > 0) then
-		surface.SetDrawColor(0, 0, 0, oopacity * 255)
-		draw.NoTexture()
-		drawCircle(startw + s / 2, starth + s / 2, dot + 2, 45)
-		surface.SetDrawColor(r, g, b, dopacity)
-		drawCircle(startw + s / 2, starth + s / 2, dot, 45)
-	end
+	
+	ttt.DefaultCrosshair(x, y)
 
 	return true
 end
--- gamemode/cl_crosshair_menu.lua
+++ gamemode/cl_crosshair_menu.lua
@@ -1,3 +1,14 @@
+local tttrw_crosshair_color_r = CreateConVar("tttrw_crosshair_color_r", 232, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
+local tttrw_crosshair_color_g = CreateConVar("tttrw_crosshair_color_g", 80, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
+local tttrw_crosshair_color_b = CreateConVar("tttrw_crosshair_color_b", 94, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
+local tttrw_crosshair_thickness = CreateConVar("tttrw_crosshair_thickness", 2, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
+local tttrw_crosshair_length = CreateConVar("tttrw_crosshair_length", 7, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
+local tttrw_crosshair_gap = CreateConVar("tttrw_crosshair_gap", 3, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
+local tttrw_crosshair_opacity = CreateConVar("tttrw_crosshair_opacity", 255, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
+local tttrw_crosshair_outline_opacity = CreateConVar("tttrw_crosshair_outline_opacity", 255, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
+local tttrw_crosshair_dot_size = CreateConVar("tttrw_crosshair_dot_size", 0, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
+local tttrw_crosshair_dot_opacity = CreateConVar("tttrw_crosshair_dot_opacity", 255, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
+
 concommand.Add("tttrw_crosshair_menu", function()
 	local frame = vgui.Create("DFrame")
 	frame:SetPos(ScrW()/2-400,ScrH()/2-250)
@@ -8,9 +19,12 @@ concommand.Add("tttrw_crosshair_menu", function()
 	frame:ShowCloseButton(true)
 	frame:MakePopup()
 
-	local crosshair = vgui.Create("ttt_crosshairs", frame)
+	local crosshair = vgui.Create("EditablePanel", frame)
 	crosshair:SetPos(25,0)
 	crosshair:SetSize(400,475)
+	function crosshair:Paint(w, h)
+		ttt.DefaultCrosshair(w / 2, h / 2)
+	end
 
 	local col = vgui.Create("DColorMixer", frame)
 	col:SetPos(450,50)
@@ -101,3 +115,40 @@ concommand.Add("tttrw_crosshair_menu", function()
 		dotop:SetValue(255)
 	end
 end)
+
+function ttt.DefaultCrosshair(x, y)
+	local r = tttrw_crosshair_color_r:GetInt()
+	local g = tttrw_crosshair_color_g:GetInt()
+	local b = tttrw_crosshair_color_b:GetInt()
+	local t = tttrw_crosshair_thickness:GetInt()
+	local len = tttrw_crosshair_length:GetInt()
+	local gap = tttrw_crosshair_gap:GetInt()*2
+	local opacity = tttrw_crosshair_opacity:GetInt()
+	local oopacity = tttrw_crosshair_outline_opacity:GetInt()
+	local dot = tttrw_crosshair_dot_size:GetInt()
+	local dopacity = tttrw_crosshair_dot_opacity:GetInt()
+
+	local s = len * 2 + gap
+	local startw = x - s / 2
+	local starth = y - s / 2
+	if (len > 0 and t > 0) then
+		surface.SetDrawColor(0, 0, 0, oopacity * 255) -- outlines, counterclockwise
+		surface.DrawRect(x - t / 2 - 1, starth - 1, t + 2, len + 2)
+		surface.DrawRect(startw - 1 , y- t / 2 - 1, len + 2, t + 2)
+		surface.DrawRect(x - t / 2 - 1, starth + s - len - 1, t + 2, len + 2)
+		surface.DrawRect(startw + s - len - 1, y - t / 2 - 1, len + 2, t + 2)
+		surface.SetDrawColor(r, g, b, opacity) -- crosshairs, counterclockwise
+		surface.DrawRect(x - t / 2, starth, t, len)
+		surface.DrawRect(startw, y - t / 2, len, t)
+		surface.DrawRect(x - t / 2, starth + s - len, t, len)
+		surface.DrawRect(startw + s - len, y - t / 2, len, t)
+	end
+
+	if (dot > 0) then
+		surface.SetDrawColor(0, 0, 0, oopacity * 255)
+		draw.NoTexture()
+		drawCircle(startw + s / 2, starth + s / 2, dot + 2, 45)
+		surface.SetDrawColor(r, g, b, dopacity)
+		drawCircle(startw + s / 2, starth + s / 2, dot, 45)
+	end
+end
