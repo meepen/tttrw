059c7cf55d2eb1cc39b3b9e273594ab19beb1a32
Meepen <meepdarknessmeep@gmail.com>
2019-08-11 17:25:47 -0400

    Move Crosshairs to swep code

--- entities/weapons/weapon_tttbase/cl_init.lua
+++ entities/weapons/weapon_tttbase/cl_init.lua
@@ -2,6 +2,7 @@ include "shared.lua"
 
 local ttt_lowered = CreateConVar("ttt_ironsights_lowered", "1", FCVAR_ARCHIVE)
 
+SWEP.DrawCrosshair = true
 SWEP.ScopeArcTexture = Material "sprites/scope_arc"
 
 function SWEP:DrawHUD()
@@ -34,6 +35,31 @@ function SWEP:DrawHUD()
 		surface.DrawRect(math.floor(x + ScrH() / 2), 0, math.ceil(x - ScrH() / 2), ScrH())
 	end
 end
+local tttrw_crosshair_color_r = CreateConVar("tttrw_crosshair_color_r", 232, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
+local tttrw_crosshair_color_g = CreateConVar("tttrw_crosshair_color_g", 80, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
+local tttrw_crosshair_color_b = CreateConVar("tttrw_crosshair_color_b", 94, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
+local tttrw_crosshair_thickness = CreateConVar("tttrw_crosshair_thickness", 2, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
+local tttrw_crosshair_length = CreateConVar("tttrw_crosshair_length", 7, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
+local tttrw_crosshair_gap = CreateConVar("tttrw_crosshair_gap", 3, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
+local tttrw_crosshair_opacity = CreateConVar("tttrw_crosshair_opacity", 255, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
+local tttrw_crosshair_outline_opacity = CreateConVar("tttrw_crosshair_outline_opacity", 255, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
+local tttrw_crosshair_dot_size = CreateConVar("tttrw_crosshair_dot_size", 0, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
+local tttrw_crosshair_dot_opacity = CreateConVar("tttrw_crosshair_dot_opacity", 255, {FCVAR_ARCHIVE, FCVAR_UNLOGGED}, "")
+
+local function drawCircle(x, y, radius, seg)
+	local cir = {}
+
+	table.insert(cir, {x = x, y = y, u = 0.5, v = 0.5})
+	for i = 0, seg do
+		local a = math.rad((i / seg) * -360)
+		table.insert(cir, {x = x + math.sin(a) * radius, y = y + math.cos(a) * radius, u = math.sin(a) / 2 + 0.5, v = math.cos(a) / 2 + 0.5})
+	end
+
+	local a = math.rad( 0 ) -- This is needed for non absolute segment counts
+	table.insert( cir, {x = x + math.sin(a) * radius, y = y + math.cos(a) * radius, u = math.sin(a) / 2 + 0.5, v = math.cos(a) / 2 + 0.5})
+
+	surface.DrawPoly(cir)
+end
 
 function SWEP:DoDrawCrosshair(x, y)
 	if (self:GetIronsights() and self.HasScope) then
@@ -49,9 +75,42 @@ function SWEP:DoDrawCrosshair(x, y)
 
 		return true
 	end
-	surface.SetDrawColor(255, 0, 255, 255)
-	surface.DrawLine(x - 5, y, x + 6, y)
-	surface.DrawLine(x, y - 5, x, y + 6)
+
+	local r = tttrw_crosshair_color_r:GetInt()
+	local g = tttrw_crosshair_color_g:GetInt()
+	local b = tttrw_crosshair_color_b:GetInt()
+	local t = tttrw_crosshair_thickness:GetInt()
+	local len = tttrw_crosshair_length:GetInt()
+	local gap = tttrw_crosshair_gap:GetInt()*2
+	local opacity = tttrw_crosshair_opacity:GetInt()
+	local oopacity = tttrw_crosshair_outline_opacity:GetInt()
+	local dot = tttrw_crosshair_dot_size:GetInt()
+	local dopacity = tttrw_crosshair_dot_opacity:GetInt()
+
+	local s = len * 2 + gap
+	local startw = x - s / 2
+	local starth = y - s / 2
+	if (len > 0 and t > 0) then
+		surface.SetDrawColor(0, 0, 0, oopacity * 255) -- outlines, counterclockwise
+		surface.DrawRect(x - t / 2 - 1, starth - 1, t + 2, len + 2)
+		surface.DrawRect(startw - 1 , y- t / 2 - 1, len + 2, t + 2)
+		surface.DrawRect(x - t / 2 - 1, starth + s - len - 1, t + 2, len + 2)
+		surface.DrawRect(startw + s - len - 1, y - t / 2 - 1, len + 2, t + 2)
+		surface.SetDrawColor(r, g, b, opacity) -- crosshairs, counterclockwise
+		surface.DrawRect(x - t / 2, starth, t, len)
+		surface.DrawRect(startw, y - t / 2, len, t)
+		surface.DrawRect(x - t / 2, starth + s - len, t, len)
+		surface.DrawRect(startw + s - len, y - t / 2, len, t)
+	end
+
+	if (dot > 0) then
+		surface.SetDrawColor(0, 0, 0, oopacity * 255)
+		draw.NoTexture()
+		drawCircle(startw + s / 2, starth + s / 2, dot + 2, 45)
+		surface.SetDrawColor(r, g, b, dopacity)
+		drawCircle(startw + s / 2, starth + s / 2, dot, 45)
+	end
+
 	return true
 end
 
@@ -136,7 +195,6 @@ function SWEP:GetViewModelPosition(pos, ang)
 
 	return pos, ang + self:GetCurrentUnpredictedViewPunch()
 end
-
 
 function SWEP:GetCurrentUnpredictedFOVMultiplier()
 	local fov, time, duration = self.FOVMultiplier or 1, self.FOVMultiplierTime or -0.1, self.FOVMultiplierDuration or 0.1
