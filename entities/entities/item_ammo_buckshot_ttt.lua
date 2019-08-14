494b1c09150f5f0a0ba8620988464bba783fba99
crossboy <itscrossboy@gmail.com>
2019-08-14 16:10:59 -0700

    Add ammo boxes

---
+++ entities/entities/item_ammo_buckshot_ttt.lua
@@ -0,0 +1,9 @@
+AddCSLuaFile()
+
+ENT.Type = "anim"
+ENT.Base = "base_ammo_ttt"
+ENT.AmmoType = "Buckshot"
+ENT.AmmoAmount = 8
+ENT.AmmoMax = 24
+ENT.Model = "models/items/boxbuckshot.mdl"
+ENT.AutoSpawnable = true
