70ab1a1e4dfbca2358e194062e0c8acd1d33804d
Meepen <meepdarknessmeep@gmail.com>
2019-08-20 20:29:33 -0400

    Fix ttt_map_settings

--- entities/entities/ttt_map_settings.lua
+++ entities/entities/ttt_map_settings.lua
@@ -12,33 +12,21 @@ function ENT:AcceptInput(name, activator, call, data)
 end
 
 function ENT:KeyValue(key, value)
-	if (key == "cbar_doors") then
-		print("[ttt_map_settings] "..key.." set to "..value..".")
-	elseif (key == "cbar_buttons") then
-		print("[ttt_map_settings] "..key.." set to "..value..".")
-	elseif (key == "cbar_other") then
-		print("[ttt_map_settings] "..key.." set to "..value..".")
-	elseif (key == "plymodel") then
-		print("[ttt_map_settings] "..key.." set to "..value..".")
-	elseif (key == "propspec_named") then
-		print("[ttt_map_settings] "..key.." set to "..value..".")
-	elseif (key == "MapSettingsSpawned" or key == "RoundEnd" or key == "RoundPreparation" or key == "RoundStart") then
+	print("[ttt_map_settings] "..key.." set to "..value..".")
+	if (key == "MapSettingsSpawned" or key == "RoundEnd" or key == "RoundPreparation" or key == "RoundStart") then
 		self:StoreOutput(key, value)
 	end
 end
 
-function ENT:Think()
-	if self.FirstThink then
-		hook.Add("TTTPrepareRound", "MapSettingsPrepareRound", function()
-			self:TriggerOutput("RoundPreparation", self)
-		end)
-		hook.Add("TTTRoundStart", "MapSettingsOutputStart", function()
-			self:TriggerOutput("RoundStart", self)
-		end)
-		hook.Add("TTTRoundEnd", "MapSettingsOutputEnd", function(win)
-			self:TriggerOutput("RoundEnd", self, ((win == "traitor" and 2) or (win == "innocent" and 3)))
-		end)
-		self:TriggerOutput("MapSettingsSpawned", self)
-		self.FirstThink = false
-	end
+function ENT:Initialize()
+	hook.Add("TTTPrepareRound", self, function(self)
+		self:TriggerOutput("RoundPreparation", self)
+	end)
+	hook.Add("TTTRoundStart", self, function(self)
+		self:TriggerOutput("RoundStart", self)
+	end)
+	hook.Add("TTTRoundEnd", self, function(self, win)
+		self:TriggerOutput("RoundEnd", self, ((win == "traitor" and 2) or (win == "innocent" and 3)))
+	end)
+	self:TriggerOutput("MapSettingsSpawned", self)
 end
