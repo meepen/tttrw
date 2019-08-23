c68997b848936ef8985aa3af7780b7db7c24b275
Meepen <meepdarknessmeep@gmail.com>
2019-08-22 20:17:46 -0400

    idk why this is happening but fixes ammo issues

--- entities/weapons/weapon_tttbase/init.lua
+++ entities/weapons/weapon_tttbase/init.lua
@@ -4,5 +4,13 @@ AddCSLuaFile "cl_init.lua"
 AddCSLuaFile "shared.lua"
 include "shared.lua"
 
+function SWEP:Equip()
+    -- TODO(meep): figure out why the fuck this is happening???
+    self:GetOwner():RemoveAmmo(24, "Pistol")
+end
+
+function SWEP:OnDrop()
+    self.Primary.DefaultClip = 0
+end
 
 -- TODO(meep): hidden weapons so people can't cheat to see weapons
