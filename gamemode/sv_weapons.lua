6adea8b55c337aa439bd5ec1ced3f3c1b4a949d4
Meepen <meepdarknessmeep@gmail.com>
2019-08-22 20:30:02 -0400

    StoredAmmo when dropping the last gun you have with ammo type

--- gamemode/sv_weapons.lua
+++ gamemode/sv_weapons.lua
@@ -1,17 +1,38 @@
 function GM:PlayerCanPickupWeapon(ply, wep)
-    local weps = ply:GetWeapons()
+	local weps = ply:GetWeapons()
 
-    for _, curwep in pairs(ply:GetWeapons()) do
-        if (wep:GetSlot() == curwep:GetSlot()) then
-            return false
-        end
-    end
+	for _, curwep in pairs(ply:GetWeapons()) do
+		if (wep:GetSlot() == curwep:GetSlot()) then
+			return false
+		end
+	end
 
-    wep:SetPos(ply:GetShootPos())
+	wep:SetPos(ply:GetShootPos())
 
-    return true
+	return true
+end
+
+function GM:WeaponEquip(wep, ply)
+	if (wep.StoredAmmo) then
+		local pri = wep:GetPrimaryAmmoType()
+		ply:SetAmmo(wep.StoredAmmo + ply:GetAmmoCount(pri), pri)
+		wep.StoredAmmo = nil
+	end
 end
 
 function GM:DropCurrentWeapon(ply)
-    ply:DropWeapon(ply:GetActiveWeapon())
+	local wep = ply:GetActiveWeapon()
+	ply:DropWeapon(wep)
+
+	local pri = wep:GetPrimaryAmmoType()
+
+	for _, otherwep in pairs(ply:GetWeapons()) do
+		if (otherwep:GetPrimaryAmmoType() == pri) then
+			return
+		end
+	end
+
+	wep.StoredAmmo = ply:GetAmmoCount(pri)
+
+	ply:SetAmmo(0, pri)
 end
