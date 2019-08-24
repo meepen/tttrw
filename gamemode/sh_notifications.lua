20e171b4813e672e2d2dfe96153d4b4e57fc3ff3
Meepen <meepdarknessmeep@gmail.com>
2019-08-24 15:39:34 -0400

    notifications better

--- gamemode/sh_notifications.lua
+++ gamemode/sh_notifications.lua
@@ -12,9 +12,11 @@ if (SERVER) then
 		net.Send(self)
 	end
 else
+
+	local textheight = math.Round(ScrH() / 80)
 	surface.CreateFont("ttt_notifications", {
 		font = 'Lato',
-		size = ScrH() / 40,
+		size = textheight,
 		weight = 200,
 	})
 
@@ -34,38 +36,40 @@ else
 		
 		table.insert(ttt.Notifications.NotificationList, notif)
 	end
-	
-	
+
 	local bg_default = Color(0x0B, 0x0C, 0x0B, 0xE0)
 	hook.Add("HUDPaint", "ttt.Notifications.HUDPaint", function()
-		local w, h = ScrW() * 0.275, 30
+		local w, h = textheight * 25, textheight * 1.5
 		local pad = h + 10
 		local x = ScrW() * 0.98 - w
-		
+
 		local to_remove = {}
-	
-		for k, notif in pairs(ttt.Notifications.NotificationList) do
-			print(notif.birth, notif.death, SysTime(), SysTime() - notif.birth, notif.death - SysTime(), (SysTime() - notif.birth)/(notif.death - SysTime()))
-			local y = ScrH() * 0.01 + pad * k
-			
+
+		for i = #ttt.Notifications.NotificationList, 1, -1 do
+			local notif = ttt.Notifications.NotificationList[i]
+
 			if (notif.death <= SysTime()) then
-				table.insert(to_remove, k)
-				continue
+				table.insert(to_remove, i)
 			end
-			
-			local col = bg_default
-			if (notif.death < SysTime() + notif.lifetime / 3) then
-				col = Color(0x0B, 0x0C, 0x0B, Lerp((SysTime() + 10) / notif.death, 0xE0, 0))
+		end
+
+		for k, notif in pairs(ttt.Notifications.NotificationList) do
+			local y = ScrH() * 0.01 + pad * k
+
+
+			local frac = (SysTime() - notif.birth) / notif.lifetime
+			local alpha = 1
+			if (frac > 0.75) then
+				alpha = 1 - (frac - 0.75) * 4
 			end
-			
-			
-			
-			
+			alpha = Lerp(alpha, 0, 0xE0)
+			local col = ColorAlpha(bg_default, alpha)
+
 			draw.RoundedBox(4, x, y, w, h, col)
 		
-			draw.SimpleText(notif.msg, "ttt_notifications", x + w/2, y + h/2 - 1, Color(0xFE, 0xFE, 0xFE), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+			draw.SimpleText(notif.msg, "ttt_notifications", x + w / 2, y + h / 2 - 1, Color(0xFE, 0xFE, 0xFE, alpha), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
 		end
-		
+
 		for _, idx in pairs(to_remove) do
 			table.remove(ttt.Notifications.NotificationList, idx)
 		end
