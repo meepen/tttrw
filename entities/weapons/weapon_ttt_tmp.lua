5b2087a0c6736dea1c77ad4041ee9ffcacc494e7
Ikthemon <mrletsplay117@gmail.com>
2019-09-07 00:11:43 -0400

    tmp bandaid

--- entities/weapons/weapon_ttt_tmp.lua
+++ entities/weapons/weapon_ttt_tmp.lua
@@ -18,14 +18,14 @@ SWEP.WeaponID              = AMMO_PISTOL
 SWEP.Bullets = {
 	HullSize = 0,
 	Num = 1,
-	DamageDropoffRange = 600,
-	DamageDropoffRangeMax = 1400,
+	DamageDropoffRange = 200,
+	DamageDropoffRangeMax = 1000,
 	DamageMinimumPercent = 0.1,
-	Spread = Vector(0.040, 0.040, 0.00)
+	Spread = Vector(0.050, 0.050)
 }
 
-SWEP.Primary.Damage        = 12
-SWEP.Primary.Delay         = 0.03
+SWEP.Primary.Damage        = 6
+SWEP.Primary.Delay         = 0.05
 SWEP.Primary.Recoil        = 1.2
 SWEP.Primary.Automatic     = true
 SWEP.Primary.Ammo          = "smg1"
@@ -43,8 +43,8 @@ SWEP.ViewModel             = "models/weapons/cstrike/c_smg_tmp.mdl"
 SWEP.WorldModel            = "models/weapons/w_smg_tmp.mdl"
 
 SWEP.Ironsights = {
-	Pos = Vector(-8.921, -9.528, 2.9),
-	Angle = Vector(0.699, -5.301, -7),
+	Pos = Vector(-2, -4, 2.9),
+	Angle = Vector(2, 2, -7),
 	TimeTo = 0.25,
 	TimeFrom = 0.15,
 	SlowDown = 0.3,
