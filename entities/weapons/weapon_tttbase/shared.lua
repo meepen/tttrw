75cfa8444f88648723caeb6c3783594cd1f5939a
Meepen <meepdarknessmeep@gmail.com>
2019-11-15 01:34:42 -0500

    remove print

--- entities/weapons/weapon_tttbase/shared.lua
+++ entities/weapons/weapon_tttbase/shared.lua
@@ -540,12 +540,11 @@ function SWEP:Think()
 	if (reloadtime ~= math.huge) then
 		if (reloadtime > CurTime()) then
 			local time = (CurTime() - self:GetReloadStartTime())
-	
+
 			local snd = IsFirstTimePredicted() and self.Sounds and self.Sounds.reload
 			if (snd) then
 				for _ = #snd, 1, -1 do
 					local inf = snd[_]
-					print(time)
 					if (inf.time / self:GetReloadAnimationSpeed() <= time) then
 						if (self.LastSound ~= inf.sound) then
 							self:EmitSound(inf.sound)
