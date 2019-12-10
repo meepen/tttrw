6298b05977ccc1552b4285b54d617f4a9b01eb20
Ikthemon <mrletsplay117@gmail.com>
2019-12-10 03:42:53 -0500

    junk deagle will 1 shot head

--- entities/weapons/weapon_ttt_deagle.lua
+++ entities/weapons/weapon_ttt_deagle.lua
@@ -33,7 +33,7 @@ SWEP.Primary.ClipSize      = 8
 SWEP.Primary.DefaultClip   = 16
 SWEP.Primary.Sound         = Sound "Weapon_Deagle.Single"
 
-SWEP.HeadshotMultiplier    = 4.6
+SWEP.HeadshotMultiplier    = 5
 SWEP.DeploySpeed = 1.55
 
 SWEP.AutoSpawnable         = true
