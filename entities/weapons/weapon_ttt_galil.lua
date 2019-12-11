ee78243279de828dc31603c408f89531dd5e541a
Meepen <meepdarknessmeep@gmail.com>
2019-12-11 00:22:08 -0500

    galil delay to 0.09

--- entities/weapons/weapon_ttt_galil.lua
+++ entities/weapons/weapon_ttt_galil.lua
@@ -18,11 +18,11 @@ SWEP.Bullets = {
 	DamageDropoffRange = 550,
 	DamageDropoffRangeMax = 3500,
 	DamageMinimumPercent = 0.1,
-	Spread = Vector(0.025, 0.025),
+	Spread = Vector(0.025, 0.02),
 }
 
 SWEP.Primary.Damage        = 17
-SWEP.Primary.Delay         = 0.1
+SWEP.Primary.Delay         = 0.09
 SWEP.Primary.Recoil        = 1.5
 SWEP.Primary.Automatic     = true
 SWEP.Primary.Ammo          = "SMG1"
