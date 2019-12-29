89a739add6dfc56d9decab452a62f95dd15a4f29
Ikthemon <mrletsplay117@gmail.com>
2019-12-29 01:51:57 -0500

    m4 hs multiplyer fix

--- entities/weapons/weapon_ttt_m4a1.lua
+++ entities/weapons/weapon_ttt_m4a1.lua
@@ -34,7 +34,7 @@ SWEP.Primary.ClipSize      = 30
 SWEP.Primary.DefaultClip   = 60
 SWEP.Primary.Sound         = Sound "Weapon_M4A1.Single"
 
-SWEP.HeadshotMultiplier    = 2.2
+SWEP.HeadshotMultiplier    = 1.7
 SWEP.DeploySpeed = 1.4
 
 SWEP.AutoSpawnable         = true
