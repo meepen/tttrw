19188d1f00eeba6a3b68053961b9ce905165fe59
Ikthemon <mrletsplay117@gmail.com>
2020-05-21 19:29:21 -0400

    fix the SNIPERS JESUS

--- entities/weapons/weapon_ttt_rifle.lua
+++ entities/weapons/weapon_ttt_rifle.lua
@@ -47,7 +47,7 @@ SWEP.AmmoEnt               = "item_ammo_357_ttt"
 SWEP.ViewModel             = "models/weapons/cstrike/c_snip_scout.mdl"
 SWEP.WorldModel            = "models/weapons/w_snip_scout.mdl"
 SWEP.HasScope              = true
-SWEP.IsSniper = true
+SWEP.IsSniper = false
 
 SWEP.Ironsights = {
 	Pos = Vector(5, -15, -2),
