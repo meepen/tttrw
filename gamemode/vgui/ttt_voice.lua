9283b0f0181ca0a3bea71d2d74a74911f6b1c1a4
Meepen <meepdarknessmeep@gmail.com>
2020-12-28 15:29:48 -0500

    different voice look

--- gamemode/vgui/ttt_voice.lua
+++ gamemode/vgui/ttt_voice.lua
@@ -18,6 +18,8 @@ function PANEL:Init()
 	self:DockPadding(4, 4, 4, 4)
 	self:DockMargin(2, 2, 2, 2)
 	self:Dock(TOP)
+
+	self.VolumeHistory = {}
 end
 
 function PANEL:Setup(ply)
@@ -30,21 +32,61 @@ function PANEL:Setup(ply)
 	self:InvalidateLayout()
 end
 
+local function LerpColors(frac, ...)
+	local amt = select("#", ...) - 1
+	frac = math.min(frac, 0.99999)
+
+	local fromn = math.floor(frac / (1 / amt)) + 1
+
+	local from, to = select(fromn, ...)
+
+	frac = (frac - (fromn - 1) * (1 / amt)) / (1 / amt)
+
+	return Color(
+		from.r + (to.r - from.r) * frac,
+		from.g + (to.g - from.g) * frac,
+		from.b + (to.b - from.b) * frac,
+		from.a + (to.a - from.a) * frac
+	)
+end
+
+local max_volume = Color(255, 50, 0, 100)
+local min_volume = Color(0, 255, 0, 100)
+local med_volume = Color(255, 255, 0, 100)
+
+
 function PANEL:Paint( w, h )
 	if (not IsValid(self.ply)) then
 		return
 	end
 
+	local history_length = 4 -- seconds
+	table.insert(self.VolumeHistory, {
+		Time = CurTime(),
+		Volume = self.ply:VoiceVolume()
+	})
+	self.VolumeHistory.MaxVolume = math.max(self.VolumeHistory.MaxVolume or 0.5, self.ply:VoiceVolume())
+	while (self.VolumeHistory[1].Time < CurTime() - history_length) do
+		table.remove(self.VolumeHistory, 1)
+	end
+
 	local col
-	if (IsValid(self.ply.HiddenState) and not self.ply.HiddenState:IsDormant() and self.ply.VoiceState and self.ply:GetRole() ~= "Innocent") then
-		col = self.ply:GetRoleData().Color
-	elseif (not self.ply:Alive()) then
-		col = Color(self.ply:VoiceVolume() * 255, self.ply:VoiceVolume() * 255, 0, 240)
+	if (IsValid(self.ply.HiddenState) and not self.ply.HiddenState:IsDormant() and (self.ply.VoiceState or not self.ply:GetRoleData().Evil) and self.ply:GetRole() ~= "Innocent") then
+		col = ColorAlpha(self.ply:GetRoleData().Color, 210)
+	elseif (LocalPlayer():Alive() or self.ply:Alive()) then
+		col = Color(23, 25, 21, 240)
 	else
-		col = Color(0, self.ply:VoiceVolume() * 255, 0, 240)
+		col = Color(100, 120, 0, 240)
 	end
 
 	draw.RoundedBox(4, 0, 0, w, h, col)
+
+	local max = self.VolumeHistory.MaxVolume
+	for _, data in ipairs(self.VolumeHistory) do
+		local frac = 1 - (CurTime() - data.Time) / history_length
+		surface.SetDrawColor(LerpColors(data.Volume / max, min_volume, med_volume, max_volume))
+		surface.DrawLine(w * frac, h, w * frac, h - h * data.Volume / max)
+	end
 end
 
 function PANEL:Think()
@@ -59,6 +101,7 @@ end
 
 function PANEL:FadeOut( anim, delta, data )
 	if (anim.Finished) then
+		self.VolumeHistory = {}
 
 		if (IsValid(PlayerVoicePanels[self.ply])) then
 			PlayerVoicePanels[self.ply]:Remove()
