b19a6ed9d0217262b246b966fb7df815274652df
Meepen <meepdarknessmeep@gmail.com>
2020-12-28 13:10:55 -0500

    animation shared

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -94,3 +94,47 @@ hook.Add("HUDPaint", "gmod_mcore_test", function()
 	hook.Remove("HUDPaint", "gmod_mcore_test")
 	timer.Simple(2, Callback)
 end)
+
+function GM:UpdateAnimation(ply, velocity, maxseqgroundspeed)
+	local len = velocity:Length()
+	local movement = 1.0
+
+	if (len > 0.2) then
+		movement = len / maxseqgroundspeed
+	end
+
+	local rate = math.min(movement, 2)
+
+	-- if we're under water we want to constantly be swimming..
+	if (ply:WaterLevel() >= 2) then
+		rate = math.max(rate, 0.5)
+	elseif (not ply:IsOnGround() and len >= 1000) then
+		rate = 0.1
+	end
+
+	ply:SetPlaybackRate(rate)
+
+	if (ply:InVehicle()) then
+		--
+		-- This is used for the 'rollercoaster' arms
+		--
+		local Vehicle = ply:GetVehicle()
+		local Velocity = Vehicle:GetVelocity()
+		local fwd = Vehicle:GetUp()
+		local dp = fwd:Dot(Vector( 0, 0, 1))
+
+		ply:SetPoseParameter("vertical_velocity", ( dp < 0 && dp || 0 ) + fwd:Dot( Velocity ) * 0.005)
+
+		-- Pass the vehicles steer param down to the player
+		local steer = Vehicle:GetPoseParameter "vehicle_steer"
+		steer = steer * 2 - 1 -- convert from 0..1 to -1..1
+		if (Vehicle:GetClass() == "prop_vehicle_prisoner_pod" ) then
+			steer = 0
+			ply:SetPoseParameter("aim_yaw", math.NormalizeAngle(ply:GetAimVector():Angle().y - Vehicle:GetAngles().y - 90))
+		end
+		ply:SetPoseParameter("vehicle_steer", steer)
+	end
+
+	self:GrabEarAnimation(ply)
+	self:MouthMoveAnimation(ply)
+end
